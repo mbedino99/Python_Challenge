c5bb8744e36b2493bfd6fbc5b6bae6c7d64d04aa
Martin Javier Bedino <tincho@Martins-MBP.attlocal.net>
2023-06-27 13:21:21 -0400

    06/27

--- Pybank/main.py
+++ Pybank/main.py
@@ -4,25 +4,29 @@ import csv
 
 # open csv file and create csv reader
 csvpath = os.path.join("/Users/tincho/Desktop/Challenge Reps/Python_Challenge/Pybank_data/budget_data.csv")
-with open(csvpath) as dataset:
-    csvreader = (csv.reader(dataset, delimiter = ','))
+with open(csvpath) as csv_file:
+    csvreader = (csv.reader(csv_file, delimiter = ','))
 
 # check if it loads: (FINALLY IT DOES JESUS) print(csvreader)
 # read header and skip first row
     csv_header = next(csvreader)
 
-#    for row in csvreader: (edit* it works)
-#        print(row)        (edit* it works)
+#    for row in csvreader:
+#        print(row[1])
+            
+# iterate throwugh rows and get the sum of months
+#    for row in csvreader:
+#         total_months = sum(row[0])
+#         print(total_months)
 
+# iterate through rows to find net total profit
+    for row in csvreader:
+        net_profit = sum(int(row[1]))
+        print(net_profit)
 
-# create the sum of months
-    ## for some reason this isnt working. will debug later 
-    total_months = 0
-    for row in csvreader(dataset):
-        total_months += int(row[1])
-    print(total_months)
 
 
     
+
+
     
-        
