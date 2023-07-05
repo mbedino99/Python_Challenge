ed7f40aebf93ccdd6a1c0e95f517b3bed320cc24
Martin Javier Bedino <tincho@Martins-MacBook-Pro.local>
2023-07-05 15:30:20 -0400

    PyBank Done

--- Pybank/main.py
+++ Pybank/main.py
@@ -2,34 +2,60 @@
 import os
 import csv
 
-# open csv file and create csv reader
+# variables to be used later
+total_months = 0
+old_profit = 0
+month_change = []
+profit_change_list = []
+max_increase = ['', 0]
+max_decrease = ['', 99999999999999999999]
+total_profit = 0
+
+# open csv file and create csv reader as list of dictionaries
 csvpath = os.path.join("/Users/tincho/Desktop/Challenge Reps/Python_Challenge/Pybank_data/budget_data.csv")
 with open(csvpath) as csv_file:
-    csvreader = (csv.reader(csv_file, delimiter = ','))
+    reader = csv.DictReader(csv_file)
 
-# check if it loads: (FINALLY IT DOES JESUS) print(csvreader)
-# read header and skip first row
-    csv_header = next(csvreader)
+    for row in reader:
 
-#    for row in csvreader:
-#        print(row[1])
-            
-# iterate throwugh rows and get the sum of months
-#    for row in csvreader:
-#         total_months = 0
-#         total_months = total_months + str(row[0])
-#    print(total_months)
+        # calc total months and total profit
+        total_months = total_months + 1
+        total_profit = total_profit + int(row['Profit/Losses'])
 
-# iterate through rows to find net total profit
-    for row in csvreader:
-        net_profit = 0
-        net_profit = net_profit + int(row[1])
-    print(net_profit)
+        # calculate profit change
+        profit_change = int(row['Profit/Losses']) - old_profit
+        old_profit = int(row['Profit/Losses'])
+        profit_change_list = profit_change_list + [profit_change]
+        month_change = month_change + [row['Date']]
 
+        # calculate max increase 
+        if (profit_change > max_increase[1]):
+            max_increase[0] = row['Date']
+            max_increase[1] = profit_change
 
+        # calculate max decrease
+        if (profit_change < max_decrease[1]):
+            max_decrease[0] = row['Date']
+            max_decrease[1] = profit_change
 
+# calculate average profit change
+profit_change = sum(profit_change_list) / len(profit_change_list)
 
-    
+# create output chart
+output = (
+    f'\nFinancial Analysis\n'
+    f'--------------------------\n'
+    f'Total Months: {total_months}\n'
+    f'total Profit: ${total_profit}\n'
+    f'Profit change: ${profit_change}\n'
+    f'Biggest Increase in Profit: {max_increase[0]} (${max_increase[1]})\n'
+    f'Biggest decrease in Profit: {max_decrease[0]} (${max_decrease[1]})\n'
+)
 
+print(output)
+
+# create text file
+with open("PyBank_Output", 'w') as txt_file:
+    txt_file.write(output)
 
     
